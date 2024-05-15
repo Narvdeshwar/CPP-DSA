6cc069d285c32a5a03974ed9200fd2e9fbbc8b05
narvdeshwar <sam.07soni@gmail.com>
2024-05-15 19:47:47 +0530

    pattern folder

---
+++ Pattern/reactangle.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    // lets start
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 10; j++)
+        {
+            cout << "@" << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
