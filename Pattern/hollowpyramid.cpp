da9bca49cb8d140f66698cd854847ddf418c1227
narvdeshwar <sam.07soni@gmail.com>
2024-05-23 19:20:02 +0530

    pattern

--- Pattern/hollowpyramid.cpp
+++ Pattern/hollowpyramid.cpp
@@ -3,26 +3,19 @@ using namespace std;
 int main()
 {
     // lets start
-    int n = 6;
+    int n = 5;
     for (int row = 0; row < n; row++)
     {
-        int totalColumn = n + 1;
+        int totalColumn = row+ 1;
         for (int col = 0; col < totalColumn; col++)
         {
-            if (row == 0 || row == n - 1 || row == 1)
+            if (row == 0 || row == n - 1 || col == 0 || row == col)
             {
-                cout << "* ";
+                cout << "*";
             }
             else
             {
-                if (col == 0 || col == totalColumn - 1)
-                {
-                    cout << "* ";
-                }
-                else
-                {
-                    cout << " ";
-                }
+                cout <<" ";
             }
         }
         cout << endl;
